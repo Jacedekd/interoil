6b5c2180b1b468a0d2452f2153136b2cf9903e6d
Aziz <azizkazanbaev@gmail.com>
2022-09-03 17:53:45 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+$(".img_svg").each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr("class");
+  var imgURL = $img.attr("src");
+  $.get(
+    imgURL,
+    function (data) {
+      var $svg = $(data).find("svg");
+      if (typeof imgClass !== "undefined") {
+        $svg = $svg.attr("class", imgClass + " replaced-svg");
+      }
+      $svg = $svg.removeAttr("xmlns:a");
+      if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+        $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"));
+      }
+      $img.replaceWith($svg);
+    },
+    "xml"
+  );
+});
+
 let language_Togler = document.querySelector(".language_Togler");
 let language = document.querySelector(".language");
 
@@ -229,26 +250,7 @@ $(".group .dropdown-menu li").click(function () {
   $(this).parents(".group").find("input").attr("value", $(this).attr("id"));
 });
 
-$(".img_svg").each(function () {
-  var $img = $(this);
-  var imgClass = $img.attr("class");
-  var imgURL = $img.attr("src");
-  $.get(
-    imgURL,
-    function (data) {
-      var $svg = $(data).find("svg");
-      if (typeof imgClass !== "undefined") {
-        $svg = $svg.attr("class", imgClass + " replaced-svg");
-      }
-      $svg = $svg.removeAttr("xmlns:a");
-      if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
-        $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"));
-      }
-      $img.replaceWith($svg);
-    },
-    "xml"
-  );
-});
+
 
 let content__blockText = document.querySelector(".content__blockText");
 let content__blockTextspan = document.querySelector(".content__blockText span");
