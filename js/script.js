f98f804a28d717d704b9fc2c1480fe166557bb07
Aziz <azizkazanbaev@gmail.com>
2022-09-05 12:54:03 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -19,6 +19,40 @@ $(".img_svg").each(function () {
   );
 });
 
+
+let radio__input = document.querySelectorAll('.radio__input');
+let header__vacancy = document.querySelector('.header__vacancy');
+let vacancy__Group2 = document.querySelectorAll('.vacancy__Group2');
+
+vacancy__Group2[0].classList.add('vacancy__active');
+
+let vacancyFunction = function(Name, Vacancy, e) {
+  if (e.labels[0].innerText === Name) {
+    let i;
+    for (i = 0; i < vacancy__Group2.length; i++) {
+      vacancy__Group2[i].classList.remove('vacancy__active');
+      if (vacancy__Group2[i].id === Vacancy) {
+        let vacancy__Group = vacancy__Group2[i];
+        vacancy__Group.classList.add('vacancy__active');
+      } else {
+      }
+    }
+  }
+}
+
+radio__input.forEach(function (e) {
+  e.onclick = function () {
+    vacancyFunction('Менеджер по продажам корпоративным клиентам', 'vacancy1', e);
+    vacancyFunction('Аналитик', 'vacancy2', e);
+    vacancyFunction('Финансовый менджер', 'vacancy3', e);
+    vacancyFunction('Экономист', 'vacancy4', e);
+    vacancyFunction('Начальник склада', 'vacancy5', e);
+    vacancyFunction('Водитель', 'vacancy6', e);
+    vacancyFunction('Грузчик', 'vacancy7', e);
+    vacancyFunction('Администратор', 'vacancy8', e);
+  }
+})
+
 let language_Togler = document.querySelector(".language_Togler");
 let language = document.querySelector(".language");
 
@@ -303,3 +337,4 @@ for (let e of checkboxes) {
 function changeTag() {
   this.checked ? checked.append(this.target) : hidden.append(this.target);
 }
+
